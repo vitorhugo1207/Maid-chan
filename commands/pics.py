8a7d1f05b74d6e2498174543fdf3182d38d151f2
vitorhugo1207 <vitorhugoalvesferreira@gmail.com>
2021-12-13 13:11:22 -0300

    Up

--- commands/pics.py
+++ commands/pics.py
@@ -16,17 +16,16 @@ class Pics(commands.Cog):
         r = str(r.text)
         r = json.loads(str(r))
         await ctx.send(r['url'])
-    
+
     @commands.command()
     # @commands.is_nsfw()
     async def hentai(self, ctx,*,category=None):
         if ctx.channel.is_nsfw() == True:
             if category == None:
                 category = choice(['waifu','neko','trap','blowjob'])
-            for x in range(3):
-                r = requests.get(f'https://api.waifu.pics/nsfw/{category}')
-                r = str(r.text)
-                r = json.loads(str(r))
+            r = requests.get(f'https://api.waifu.pics/nsfw/{category}')
+            r = str(r.text)
+            r = json.loads(str(r))
             await ctx.send(r['url'])
         else:
             await ctx.send('M-master, wrong channel!')
