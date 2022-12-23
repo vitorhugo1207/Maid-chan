59d40d30cfaf975a125553c5169d6fa85e0a7c21
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2022-12-23 11:12:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,62 @@
-from http import client
-import imp
-from pydoc import cli
-import discord
-from discord import activity
+import discord, sys, subprocess
 from discord.ext import commands
-import json
-import os
-import time
-import asyncio
-from sys import platform
+import config
 
-if platform == "linux" or platform == "linux2":
-    os.system('clear')
-if platform == "darwin":
-    os.system('clear')
-if platform == "win32":
-    os.system('cls')
+def clear_console():
+  if sys.platform == "linux" or sys.platform == "linux2":
+    subprocess.run("clear")
+  elif sys.platform == "darwin":
+    subprocess.run("clear")
+  elif sys.platform == "win32":
+    subprocess.run("cls")
 
-with open('config.json') as r:
-  infos = json.load(r)
-  prefix = infos['prefix']
-  token = infos['token']
-  creator = infos['creator']
-
-
-Maidchan = discord.ext.commands.Bot(command_prefix=prefix, case_insensitive=True, intents=discord.Intents.all())
+Maidchan = commands.Bot(command_prefix=config.prefix, case_insensitive=True, intents=discord.Intents.all())Maidchan = commands.Bot(command_prefix=config.prefix, case_insensitive=True, intents=discord.Intents.all())
 
 @Maidchan.event
 async def on_ready():
   print(f"{'-'*10}Bot on{'-'*10}")
-  print(f'Hi my master, I am here!')
+  print(f'Heya Master, I am here!')
   print(f'My name is {Maidchan.user.name}.')
   print(f'My user id is {Maidchan.user.id}.')
   print(f"{'-'*10}IkkiArtz{'-'*10}")
   await Maidchan.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='you!'))
 
-async def commandInit():
+async def command_init():
   print(f"{'-'*10}Commands Loading{'-'*10}")
   for filename in os.listdir('./commands'):
-    if os.path.isdir(f'./commands/{filename}') == True:
-      print(f'Unable to load dir "{filename}".')
-    else:
-        if filename.endswith('.py'):
-          await Maidchan.load_extension(f'commands.{filename[:-3]}')
-          print(f'{filename[:-3]} loaded')
-        else:
-          print(f'Unable to load {filename}.')
-
-# async def load_extensions():
-#     for filename in os.listdir("./cogs"):
-#         if filename.endswith(".py"):
-#             # cut off the .py from the file name
-#             await client.load_extension(f"cogs.{filename[:-3]}")
+    if os.path.isdir(f'./commands/{filename}') or not filename.endswith('.py'):
+      continue
+    await Maidchan.load_extension(f'commands.{filename[:-3]}')
+    print(f'{filename[:-3]} loaded')
 
 @Maidchan.command(hidden=True)
 async def load(ctx, *, filename):
-    if str(ctx.author.id) == str(creator):
-        await Maidchan.load_extension(f'commands.{filename}')
-        await ctx.send(f'My master, the {filename} commands was loaded')
-    else:
-        await ctx.send('Sorry master, only creator may loaded a module.')
-        
+  if str(ctx.author.id) == str(config.creator):
+    await Maidchan.load_extension(f'commands.{filename}')
+    await ctx.send(f'My master, the {filename} commands was loaded')
+  else:
+    await ctx.send('Sorry master, only creator may load a module.')
+
 @Maidchan.command(hidden=True)
 async def unload(ctx, *, filename):
-    if str(ctx.author.id) == str(creator):
-        await Maidchan.unload_extension(f'commands.{filename}')
-        await ctx.send(f'My master, the {filename} commands was unloaded')
-    else:
-        await ctx.send('Sorry master, only creator may unloaded a module.')
+  if str(ctx.author.id) == str(config.creator):
+    await Maidchan.unload_extension(f'commands.{filename}')
+    await ctx.send(f'My master, the {filename} commands was unloaded')
+  else:
+    await ctx.send('Sorry master, only creator may unloaded a module.')
 
 @Maidchan.command(hidden=True)
 async def reload(ctx, *, filename):
-    if str(ctx.author.id) == str(creator):
-        await Maidchan.reload_extension(f'commands.{filename}')
-        await ctx.send(f'My master, the {filename} commands was reloaded')
-    else:
-        await ctx.send('Sorry master, only creator may reloaded a module.')
+  if str(ctx.author.id) == str(config.creator):
+    await Maidchan.reload_extension(f'commands.{filename}')
+    await ctx.send(f'My master, the {filename} commands was reloaded')
+  else:
+    await ctx.send('Sorry master, only creator may reloaded a module.')
 
 async def main():
   async with Maidchan:
-    await commandInit()
-    await Maidchan.start(token)
+    await command_init()
+    await Maidchan.start(config.token)
 
+clear_console()
 asyncio.run(main())
