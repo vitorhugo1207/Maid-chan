e98887bf2cb5bbbbe38c66c3a7d6dc00af91f92b
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2022-12-23 20:05:55 +0530

    Update recreation.py

--- commands/recreation.py
+++ commands/recreation.py
@@ -3,15 +3,15 @@ from discord.ext import commands
 from commands.engine.dice.Dice import *
 
 class Recreation(commands.Cog):
-    def __init__(self, Maidchan):
-        self.Maidchan = Maidchan
+  def __init__(self, Maidchan):
+    self.Maidchan = Maidchan
 
-    @commands.command()
-    async def dice(self, ctx, *, dice):
-        diceImport = diceMain(dice)
-        diceImport.main()
-        await ctx.send(f'{dice}\n{diceImport.rollList} -> {diceImport.rollTotal}')
+  @commands.command()
+  async def dice(self, ctx, *, dice):
+    diceImport = diceMain(dice)
+    diceImport.main()
+    await ctx.send(f'{dice}\n{diceImport.rollList} -> {diceImport.rollTotal}')
 
 
 async def setup(Maidchan):
-    await Maidchan.add_cog(Recreation(Maidchan))
+  await Maidchan.add_cog(Recreation(Maidchan))
